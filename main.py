9a4d7fbe0786e4e4c6ef75bf7d4d610799a29776
vihutr <vihutr@gmail.com>
2025-02-22 20:46:46 -0800

    converted to using opengl

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from dotenv import load_dotenv
 import os
+from array import array
+
+from dotenv import load_dotenv
+import moderngl as mgl
 import pygame as pg
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -8,6 +11,13 @@ from parser import ParsedCurrentlyPlaying
 from ui_elements import ProgressBar, render_info
 from config import SETTINGS
 
+
+def surf_to_texture(surf: pg.Surface):
+    tex = ctx.texture(surf.get_size(), 4)
+    tex.filter = (mgl.NEAREST, mgl.NEAREST)
+    tex.swizzle = 'BGRA'
+    tex.write(surf.get_view('1'))
+    return tex
 
 load_dotenv()
 
@@ -25,9 +35,11 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 
 result = sp.currently_playing()
 # current_playlist = url['external_urls']['spotify']
+
+win_flags = pg.RESIZABLE | pg.OPENGL | pg.DOUBLEBUF
+
 pg.init()
 pg.font.init()
-win_flags = pg.RESIZABLE
 info = pg.display.Info()
 screen_w = info.current_w
 screen_h = info.current_h
@@ -36,9 +48,53 @@ win_x = screen_w - SETTINGS.win_size[0]
 win_y = screen_h - 70 - SETTINGS.win_size[1]
 os.environ['SDL_VIDEO_WINDOW_POS'] = f'{win_x}, {win_y}'
 
-window = pg.display.set_mode(SETTINGS.win_size, flags=win_flags)
+screen = pg.display.set_mode(SETTINGS.win_size, flags=win_flags)
+window = pg.Surface(screen.get_size())
 pg.display.set_caption('pyg-spotify')
 clock = pg.time.Clock()
+
+ctx = mgl.create_context()
+quad_buffer = ctx.buffer(data=array('f', [
+    # position (x, y), uv coords (x, y)
+    -1.0, 1.0, 0.0, 0.0,    # topleft
+    1.0, 1.0, 1.0, 0.0,     # topright
+    -1.0, -1.0, 0.0, 1.0,   # bottomleft
+    1.0, -1.0, 1.0, 1.0,    # bottomright
+]))
+
+vert_shader = '''
+#version 330 core
+
+in vec2 vert;
+in vec2 texcoord;
+out vec2 uvs;
+
+void main() {
+    uvs = texcoord;
+    gl_Position = vec4(vert, 0.0, 1.0);
+}
+'''
+
+frag_shader = '''
+#version 330 core
+
+uniform sampler2D tex;
+
+in vec2 uvs;
+out vec4 f_color;
+
+void main() {
+    f_color = vec4(texture(tex, uvs).rgb, 1.0);
+}
+'''
+
+
+program = ctx.program(vertex_shader=vert_shader, fragment_shader=frag_shader)
+render_obj = ctx.vertex_array(program, [(quad_buffer, '2f 2f', 'vert', 'texcoord')])
+
+
+
+
 
 running = True
 pressed = False
@@ -47,7 +103,6 @@ timer = 0
 seconds = 0
 current = ParsedCurrentlyPlaying(result)
 p_bar = ProgressBar()
-p_bar.highlight = True
 p_bar.fill_bar(current.progress_percent)
 p_bar.update()
 
@@ -106,6 +161,13 @@ while running:
         render_info(window, (current.song.name_text, current.album.name_text, current.song.artist_text))
         window.blit(current.progress_text, current.progress_text_rect)
         window.blit(current.duration_text, current.duration_text_rect)
+    
+    frame_tex = surf_to_texture(window)
+    frame_tex.use(0)
+    program['tex'] = 0
+    render_obj.render(mode=mgl.TRIANGLE_STRIP)
 
     pg.display.flip()
+
+    frame_tex.release()
 pg.quit()
